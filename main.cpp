f777ba04d6a183ce005fe24f6c946e13c6f761e0
Benoit Bayol <benoit.bayol@gmail.com>
2013-05-17 22:49:52 +0200

    added a test for using a predicate

--- main.cpp
+++ main.cpp
@@ -8,5 +8,13 @@ int factorial(const int n){
 
 int main(){
 	std::cout << factorial(5) << std::endl;
+
+        std::vector<double> v(4,0);
+        v[0] = 0;
+        v[1] = 6.4;
+        v[2] = 3.5;
+        v[3] = 5.7;
+        //utilisation d'un predicat more_than_3
+        std::find_if(v.begin(), v.end(), );
 }
 
