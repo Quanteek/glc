95553267066dabfda0ff0941068062e30269319c
Benoit Bayol <benoit.bayol@gmail.com>
2013-05-18 16:04:51 +0200

    reindented ; deleted useless white space ; renamed tartempion to argv

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@
 #include <cstdlib>
 
 int factorial(const int n){
-	if (n == 1)
-		return 1;
-	return n * factorial(n-1);
+    if (n == 1)
+        return 1;
+    return n * factorial(n-1);
 }
 
 struct more_than_u
 {
-	double m_u;
+    double m_u;
 
-	more_than_u(double const & u) :	m_u(u)
-	{
-		std::cout << "initialize more_than_u at " << u << "\n";
-	}
+    more_than_u(double const & u) :	m_u(u)
+    {
+        std::cout << "initialize more_than_u at " << u << "\n";
+    }
 
-	bool operator()(double n)
-	{
-		return n > m_u;
-	}
+    bool operator()(double n)
+    {
+        return n > m_u;
+    }
 
 };
 
@@ -32,19 +32,19 @@ struct more_than_u
  */
 template< typename T > T increment_before( T & t )
 {
-	return increment(t);
+    return increment(t);
 }
 
-template< typename T > 
+template< typename T >
 T increment_after( T & t )
 {
-	T tmp = t;
-	increment(t);
-	return tmp;
+    T tmp = t;
+    increment(t);
+    return tmp;
 }
 
-int main(int argc, char** tartanpion){
-	std::cout << factorial(5) << std::endl;
+int main(int argc, char** argv){
+    std::cout << factorial(5) << std::endl;
 
     std::vector<double> v(4,0);
     v[0] = 0;
@@ -52,22 +52,21 @@ int main(int argc, char** tartanpion){
     v[2] = 2.1;
     v[3] = 5.7;
     //utilisation d'un predicat more_than_3
-	more_than_u more_than = more_than_u(std::atof(tartanpion[1]));
-//    std::vector<double>::iterator it = std::find_if(v.begin(), v.end(), more_than_u(std::atof(tartanpion[1]))); 
-	std::vector<double>::iterator it = std::find_if(v.begin(), v.end(), more_than);
-	
+    more_than_u more_than = more_than_u(std::atof(argv[1]));
+    //    std::vector<double>::iterator it = std::find_if(v.begin(), v.end(), more_than_u(std::atof(argv[1])));
+    std::vector<double>::iterator it = std::find_if(v.begin(), v.end(), more_than);
 
-// Pourquoi boucle infinie de merde? Pourquoi la condition d'arrêt n'arrive jamais?
-//	for (; it != v.end(); ++it) 
-//	{
-//		it = std::find_if(it, v.end(), more_than);
-//		std::cout << *(it) << "\n";	
-//	}
 
-	while (it != v.end())
+    //Pourquoi boucle infinie de merde? Pourquoi la condition d'arrêt n'arrive jamais?
+    for (; it != v.end(); ++it)
 	{
-		std::cout <<  *(it) << "\n";
-		it = std::find_if(++it, v.end(), more_than);
+            it = std::find_if(it, v.end(), more_than);
+            std::cout << *(it) << "\n";
+	}
+
+    while (it != v.end())
+	{
+            std::cout <<  *(it) << "\n";
+            it = std::find_if(++it, v.end(), more_than);
 	}
 }
-
