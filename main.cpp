c2da91e98b0e6732cc32674153dea612bb3bd962
gui lee <guilee@gmail.com>
2013-05-18 12:51:02 +0200

    Problem with for loop. See commented code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
 
 int factorial(const int n){
 	if (n == 1)
@@ -8,13 +9,27 @@ int factorial(const int n){
 	return n * factorial(n-1);
 }
 
-bool more_than_3(double n)
+struct more_than_u
 {
-	return n > 3;
-}
+	double m_u;
+
+	more_than_u(double const & u) :	m_u(u)
+	{
+		std::cout << "initialize more_than_u at " << u << "\n";
+	}
+
+	bool operator()(double n)
+	{
+		return n > m_u;
+	}
+
+};
 
 
-/* increment() n'existe pas */
+/* increment(T & t) n'existe pas
+ * A titre d'exemple pour comprendre l'intérêt de ++i par rapport à i++
+ * Il est préférable de toujours utiliser ++i
+ */
 template< typename T > T increment_before( T & t )
 {
 	return increment(t);
@@ -28,19 +43,31 @@ T increment_after( T & t )
 	return tmp;
 }
 
-int main(){
+int main(int argc, char** tartanpion){
 	std::cout << factorial(5) << std::endl;
 
-        std::vector<double> v(4,0);
-        v[0] = 0;
-        v[1] = 6.4;
-        v[2] = 2.1;
-        v[3] = 5.7;
-        //utilisation d'un predicat more_than_3
-        std::vector<double>::iterator it = std::find_if(v.begin(), v.end(), more_than_3);
-	for (; it != v.end(); it++) 
+    std::vector<double> v(4,0);
+    v[0] = 0;
+    v[1] = 6.4;
+    v[2] = 2.1;
+    v[3] = 5.7;
+    //utilisation d'un predicat more_than_3
+	more_than_u more_than = more_than_u(std::atof(tartanpion[1]));
+//    std::vector<double>::iterator it = std::find_if(v.begin(), v.end(), more_than_u(std::atof(tartanpion[1]))); 
+	std::vector<double>::iterator it = std::find_if(v.begin(), v.end(), more_than);
+	
+
+// Pourquoi boucle infinie de merde? Pourquoi la condition d'arrêt n'arrive jamais?
+//	for (; it != v.end(); ++it) 
+//	{
+//		it = std::find_if(it, v.end(), more_than);
+//		std::cout << *(it) << "\n";	
+//	}
+
+	while (it != v.end())
 	{
-		std::cout << *(it) << std::endl;	
+		std::cout <<  *(it) << "\n";
+		it = std::find_if(++it, v.end(), more_than);
 	}
 }
 
